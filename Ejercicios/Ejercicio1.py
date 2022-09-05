24824b6eeec0425a7c8cea47abc2f270b6a72d63
Jorge <jorge.toledo.6@hotmail.com>
2022-09-05 11:48:29 -0300

    Ejercicios

---
+++ Ejercicios/Ejercicio1.py
@@ -0,0 +1,4 @@
+#Identificar y generar los modelos de clases para crear un sistema para administrar las atenciones medicas
+#de una consulta.
+#Es decir, controlar los medicos que trabajan, las horas medicas, a que pacientes atienden, y el detalle de cada consulta
+#que realizan.
---
+++ Ejercicios/Ejercicio2.py
@@ -0,0 +1,3 @@
+#Identificar y generar los modelos de clases para crear un sistema para administrar una Universidad
+#La idea es gestionar la informacion de los alumnos que se matriculan, las carreras que imparten, 
+#las asignaturas de estas carreras, los docentes, jefes de carrera, secciones, etc
