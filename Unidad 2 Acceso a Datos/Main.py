1204022c68bdbad24a73b5aff4bcf17433875061
Jorge <jorge.toledo.6@hotmail.com>
2022-11-07 12:27:27 -0300

    BD

--- Unidad 2 Acceso a Datos/Main.py
+++ Unidad 2 Acceso a Datos/Main.py
@@ -1,3 +1,15 @@
 from Model.MysqlConnection import *
 
 Dao = DAO()
+
+while True:
+    print("[1] - Gestion Items")
+    print("[2] - Gestion Personajes")
+    opcion = input("Seleccione Opcion: ")
+
+    if(opcion == "1"):
+        while True:
+            print("[1] - Ver Items")
+            print("[2] - Crear Nuevo Item")
+            print("[3] - Actualizar un Item")
+            print("[4] - Eliminar Item")
